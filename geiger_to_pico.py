87e378b3bc4894150306a81e94bc7a38705acf7e
Jennnnnnnnnnnnnnnnnnnnnnnn <92744014+Jennnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2022-03-09 13:51:33 +0000

    update to test comm branch
    added in selection of character 16 and 17 (cpm values), changed this string to be represented as an integer value.

--- geiger_to_pico.py
+++ geiger_to_pico.py
@@ -5,5 +5,7 @@ uart = UART(0, 9600)                         # init with given baudrate
 
 
 while True:                                  # start while loop
-    buffer=uart.readline()                   # prints the csv 
+    buffer=uart.readline(16,17)                   # prints the csv 
     sleep(1)                                 # puts the geiger to sleep 
+    num=str(buffer)
+    num=int
