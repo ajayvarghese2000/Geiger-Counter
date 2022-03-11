34505f3b805d9890d35b1742e22687cea2e92913
Jennnnnnnnnnnnnnnnnnnnnnnn <92744014+Jennnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2022-03-11 14:00:08 +0000

    Update geiger_to_pico.py

--- geiger_to_pico.py
+++ geiger_to_pico.py
@@ -2,10 +2,16 @@ from machine import UART                     # imports machine library
 from time import sleep                       # imports sleep library
 uart = UART(0, 9600)                         # init with given baudrate
 
-
+def extract_uvs(a):                                    # define function to extract required data
+    i = 0                                              # starts counter at 0 
+    for char in a:                                     # for loop
+        if char == str('r'):                           # if loop
+            data = a[i+3] + a[i+4] + a[i+5] + a[i+6]   # increments characters required
+            return data                                # end process
+        i = i+1                                        # increment i
 
 while True:                                  # start while loop
-    buffer=uart.readline(16,17)                   # prints the csv 
-    sleep(1)                                 # puts the geiger to sleep 
-    num=str(buffer)
-    num=int
+    geiger_data=uart.readline()              # put the serial data from the geiger into a variable
+    output = data_process(str(geiger_data))  # define output as varibale
+    print (output)                           # prints output
+    sleep(1)                                 # puts the geiger to sleep 1 second
